dce8684c65b5ad6e1162e4e6be33daaa7523b507
Adeyemi-Favour <adeyemifavour844@gmail.com>
2025-02-05 21:35:43 +0100

    first commit

---
+++ dist/actions.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// Simple JavaScript for interactivity (e.g., smooth scrolling or animations)
+document.querySelectorAll('a[href^="#"]').forEach(function (anchor) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
